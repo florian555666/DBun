95a5b474d5bb79d377be6c5212c994098bcf7b3a
florian555666 <florian.partsch@web.de>
2025-04-22 23:15:16 +0200

    d

--- DBun/obj/Debug/net9.0/DBun.AssemblyInfo.cs
+++ DBun/obj/Debug/net9.0/DBun.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DBun")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b7176cbc0868379b709e73370ca61a37eb78d30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7e41965c442550f4870241979746e157d6c61e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DBun")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DBun")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
